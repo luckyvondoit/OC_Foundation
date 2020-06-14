9c802143150c36ccb1113f579a9e4c33b0d0c4fb
luckyvon <455295147@qq.com>
2020-06-14 22:31:34 +0800

    test

---
+++ IFXGCD.h
@@ -0,0 +1,24 @@
+//
+//  IFXGCD.h
+//  IFXFramework
+//
+//  Created by luckyvon on 2020/5/2.
+//  Copyright © 2020 luckyvon. All rights reserved.
+//
+
+/*
+ 为什么我们要使用 GCD 呢？
+ */
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IFXGCD : NSObject
+
++ (void)test;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
